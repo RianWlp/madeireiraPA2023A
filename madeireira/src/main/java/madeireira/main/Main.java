8fe0026e4ddcce0f312daccfcc9d3175558e23da
rianwlp <rianwlp@gmail.com>
2023-05-13 22:51:29 +0000

    first commit2

---
+++ madeireira/src/main/java/madeireira/main/Main.java
@@ -0,0 +1,14 @@
+package madeireira.main;
+
+/**
+ *
+ * @author rianwlp
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hello World!");
+    }
+}
---
+++ madeireira/src/main/java/madeireira/negocio/Pessoa.java
@@ -0,0 +1,48 @@
+package madeireira.negocio;
+
+/**
+ *
+ * @author rianwlp
+ */
+
+import br.univates.raiz.auth.User;
+
+/**
+ * 
+ * Sera cadastrado os clientes e fornecedores
+ * 
+ */
+
+public class Pessoa implements User{
+
+    
+    /**
+     * 
+     * NOTAS 
+     *  - Ver sobre o operador que ira acessar os cadastros
+     *  - Funcionario, Cliente e Fornecedor
+     *  -
+     * 
+     */
+    
+    private int id;
+    private String username;
+    private String nome;
+    private String password;
+    
+    @Override
+    public String getUsername() {
+        
+    }
+
+    @Override
+    public String getName() {
+        
+    }
+
+    @Override
+    public String getPassword() {
+     
+        return 
+    }
+}
